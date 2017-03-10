34cda06f1caa36e1a0b596be3232b061606db0d8
Omer <omershakked@gmail.com>
2017-03-10 16:32:31 +0200

    Updated ping pong code

--- arduino/UDPSendReceiveString__ENC28J60/UDPSendReceiveString__ENC28J60.ino/UDPSendReceiveString__ENC28J60.ino.ino
+++ arduino/UDPSendReceiveString__ENC28J60/UDPSendReceiveString__ENC28J60.ino/UDPSendReceiveString__ENC28J60.ino.ino
@@ -1,81 +1,70 @@
 #include <UIPEthernet.h>
+#define UDP_TX_PACKET_MAX_SIZE 100
 
 EthernetUDP udp;
 unsigned long next;
-IPAddress pongIP(192, 168, 36, 142);
+IPAddress pongIP(192, 168, 0, 1);
+//IPAddress selfIP(192, 168, 0, 1);
+IPAddress selfIP(192,168,0,6);
+//byte myMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x00 };
+byte myMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01 };
+
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+char  originalPing[] = "Ping"; 
+unsigned int localPort = 8888;
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
 void setup() {
 
   Serial.begin(9600);
+  
+  Ethernet.begin(myMac, selfIP);
 
-  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-  Serial.println("aaa");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for (;;)
-      ;
-  }
   Serial.println(Ethernet.localIP());
-  next = millis()+5000;
+
+  
+  if (Ethernet.localIP() !=  pongIP){
+    Serial.println("Sending data to pong");
+    udp.beginPacket(pongIP, localPort);
+    udp.write(originalPing);
+    udp.endPacket();
+    udp.stop();
+  }
+  
 }
 
 void loop() {
 
   int success;
   int len = 0;
-
-  if (((signed long)(millis()-next))>0)
-    {
-      do
-        {
-          success = udp.beginPacket(IPAddress(192,168,0,1),5000);
-          Serial.print("beginPacket: ");
-          Serial.println(success ? "success" : "failed");
-          //beginPacket fails if remote ethaddr is unknown. In this case an
-          //arp-request is send out first and beginPacket succeeds as soon
-          //the arp-response is received.
-        }
-      while (!success && ((signed long)(millis()-next))<0);
-      if (!success )
-        goto stop;
-
-      success = udp.write("hello world from arduino");
-
-      Serial.print("bytes written: ");
-      Serial.println(success);
-
-      success = udp.endPacket();
-
-      Serial.print("endPacket: ");
-      Serial.println(success ? "success" : "failed");
-
-      do
-        {
-          //check for new udp-packet:
-          success = udp.parsePacket();
-        }
-      while (!success && ((signed long)(millis()-next))<0);
-      if (!success )
-        goto stop;
-
-      Serial.print("received: '");
-      do
-        {
-          int c = udp.read();
-          Serial.write(c);
-          len++;
-        }
-      while ((success = udp.available())>0);
-      Serial.print("', ");
-      Serial.print(len);
-      Serial.println(" bytes");
-
-      //finish reading this packet:
-      udp.flush();
-
-      stop:
-      udp.stop();
-      next = millis()+5000;
+  int packetSize = udp.parsePacket();
+  Serial.println("Listening...");
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = udp.remoteIP();
+    for (int i = 0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
     }
+    Serial.print(", port ");
+    Serial.println(udp.remotePort());
+
+    // read the packet into packetBufffer
+    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+
+    // send a reply to the IP address and port that sent us the packet we received
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write(ReplyBuffer);
+    udp.endPacket();
+  }
+  udp.stop();
+  
+  delay(1000);
 }
